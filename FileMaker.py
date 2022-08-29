5d07de7dac4d98ed2a05641ca6a4821b2990e515
Jens Marissens <76743319+JensMarissens@users.noreply.github.com>
2022-08-29 15:53:22 +0200

    Update FileMaker.py

--- FileMaker.py
+++ FileMaker.py
@@ -2,11 +2,11 @@ import os
 
 def addFile():
     invoer = input("Enter preferred file name: ")
-    extention = input("Choose file extention type: ")
-    fileName = invoer + extention
+    extention = input("Choose file extention type (without dot): ")
+    fileName = invoer + "." + extention
 
-    cDir = os.listdir("C:\\Users\\The Hub\\Documents")
-    if fileName in cDir:
+    cDir = os.listdir("C:\\Users\\The Hub\\Documents") #collect all files in Documents folder
+    if fileName in cDir: #check if the filename already exists for the specified extention type if yes, get new input, if no create file
         invoer = input("A file with this name already exists, please provide another: ")
         fileName = invoer + extention
         file = open(fileName, "w+")
@@ -19,13 +19,13 @@ def addFolder():
     if invoer.lower() == 'y':
         folderName = input("Folder name: ")
 
-        cDir = os.listdir("C:\\Users\\The Hub\\Documents")
-        if folderName in cDir:
+        cDir = os.listdir("C:\\Users\\The Hub\\Documents") 
+        if folderName in cDir: #check for identical folder names
             folderName = input("This folder already exists, provide a different name: ")
-            os.mkdir(folderName)
+            os.mkdir(folderName) #create folder
         else:
             os.mkdir(folderName)
-        os.chdir(r"C:\Users\The Hub\Documents\\" + folderName)
+        os.chdir(r"C:\Users\The Hub\Documents\\" + folderName) #auto redirect to the newly created folder
         addFile()
     elif invoer.lower() == 'n':
         addFile()
